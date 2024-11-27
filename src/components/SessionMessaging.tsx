df6922cbeb939b79700d23165661f967fe545ac5
Asta <s.a.moizkhan988@gmail.com>
2024-11-27 23:19:25 +0500

    Fixed Issues

--- src/components/SessionMessaging.tsx
+++ src/components/SessionMessaging.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useUser } from '@clerk/nextjs';
 import { sendMessage, supabase } from '@/utils/messaging';
 import { useParams } from 'next/navigation';
