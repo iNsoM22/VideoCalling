f3b3fb3a72a2b176b52b245214aef499b3d2bbd6
Asta <s.a.moizkhan988@gmail.com>
2025-05-06 10:24:02 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <ClerkProvider
-        
+        onUserDelete='/sign-in'
         appearance={{
           layout: {
             socialButtonsVariant: 'iconButton',
